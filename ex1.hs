31b0717d98f13d415311f322e5131c2278e3801a
Michal Staniaszek <m.staniaszek@gmail.com>
2011-10-30 19:40:26 +0900

    q2 done

--- ex1.hs
+++ ex1.hs
@@ -7,3 +7,6 @@ sotls x y z = sum (map square (delete (minimum [x,y,z]) [x,y,z]))
 square x = x * x
 
 -- 2
+sumsq :: Int -> Int
+sumsq x | x > 0 = sum (map square [1..x])
+        | otherwise = error "integer must be positive"
