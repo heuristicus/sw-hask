5a8e2d738fbb1ae98d4ffc497119c59afca7b8da
Michal Staniaszek <m.staniaszek@gmail.com>
2011-10-30 20:02:07 +0900

    q3 done, updated function defs to prevent errors

--- ex1.hs
+++ ex1.hs
@@ -1,12 +1,17 @@
 import Data.List
 
 -- 1
-sotls :: Int -> Int -> Int -> Int
+sotls :: Integer -> Integer -> Integer -> Integer
 sotls x y z = sum (map square (delete (minimum [x,y,z]) [x,y,z]))
 
 square x = x * x
 
 -- 2
-sumsq :: Int -> Int
+sumsq :: Integer -> Integer
 sumsq x | x > 0 = sum (map square [1..x])
         | otherwise = error "integer must be positive"
+
+-- 3
+fact :: Integer -> Integer
+fact x | x > 0 = product [1..x]
+       | otherwise = error "integer must be positive"
