da5b1a5bc272040da74a07d77218960aab4c2272
Michal Staniaszek <m.staniaszek@gmail.com>
2011-11-01 16:13:31 +0900

    2.2 done

--- ex2.hs
+++ ex2.hs
@@ -9,3 +9,11 @@ productList2 :: [Int] -> Int
 productList2 x = foldr (*) 1 x
 
 -- 2
+myand :: [Bool] -> Bool
+myand [] = True
+myand (x:xs) = x && myand xs
+
+-- or
+
+myand2 :: [Bool] -> Bool
+myand2 x = foldr (&&) True x
